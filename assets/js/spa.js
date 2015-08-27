96f996bde4cf54cf41c7f505a8e01878f88b7a0d
soumik652 <soumik652@hotmail.com>
2015-08-27 09:43:43 +0530

    Mon Aug 24 02:20:55 IST 2015

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,13 +1,18 @@
 
   angular.module('student',[]);
-  angular.module('student').controller('BaseCtrl',['$scope',function($scope){
-      $scope.things=[{
-         name:'soumik',
-         college:'NSEC',
-         stream:'CSE'
-      },{
-          name:'shakya',
-         college:'RCCIIT',
-         stream:'IT'  
-      }]     
+  angular.module('student').controller('BaseCtrl',['$scope','$http',function($scope,$http){
+      $http.get('/student').success(function(response){
+        $scope.things=response;
+      })
+      
+      
+//      $scope.things=[{
+//         name:'soumik',
+//         college:'NSEC',
+//         stream:'CSE'
+//      },{
+//          name:'shakya',
+//         college:'RCCIIT',
+//         stream:'IT'  
+//      }]     
   }]);
